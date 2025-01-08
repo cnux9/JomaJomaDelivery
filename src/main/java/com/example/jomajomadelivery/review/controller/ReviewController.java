bca4760be06585638174b6a14bad7c62f9f14f87
cnux9 <cnux9@naver.com>
2025-01-08 16:16:16 +0900

    refactor: add annotations

--- src/main/java/com/example/jomajomadelivery/review/controller/ReviewController.java
+++ src/main/java/com/example/jomajomadelivery/review/controller/ReviewController.java
@@ -6,11 +6,11 @@ import com.example.jomajomadelivery.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/stores/{storeId}/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
