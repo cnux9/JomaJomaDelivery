cae3389b1cccc6ffab6926001ccf70d52ca061a4
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-01-08 17:28:04 +0900

    refactor: store엔티티 카테고리 enum적용 및 빌더패턴 적용, dto 레코드로 변경

--- src/main/java/com/example/jomajomadelivery/store/service/StoreService.java
+++ src/main/java/com/example/jomajomadelivery/store/service/StoreService.java
@@ -3,6 +3,8 @@ package com.example.jomajomadelivery.store.service;
 import com.example.jomajomadelivery.store.dto.request.StoreRequestDto;
 import com.example.jomajomadelivery.store.entity.Store;
 import com.example.jomajomadelivery.store.repository.StoreRepository;
+import com.example.jomajomadelivery.user.entity.Role;
+import com.example.jomajomadelivery.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,9 @@ import org.springframework.stereotype.Service;
 public class StoreService {
     private final StoreRepository storeRepository;
     public void addStore(StoreRequestDto dto) {
-        Store store = new Store();
-        //todo: userid대신 유저 객체(로그인한 관리자)넣어야함.
-        store.addStore(1L,dto);
+        User user = new User(1L,"aa","aa","dd", Role.SELLER,"dd","dd",false);
+        Store store =Store.addStore(user,dto);
+        //Todo dto에서 세부 주소를 따로 받고 store 엔티티 수정해야함.
         storeRepository.save(store);
     }
 }
