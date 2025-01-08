1943785d40e152a1c7be1a2684c21afdb9029e9d
Lustella-123 <tella4164@gmail.com>
2025-01-08 12:07:09 +0900

    refactor: add Column

--- src/main/java/com/example/jomajomadelivery/menu/entity/Menu.java
+++ src/main/java/com/example/jomajomadelivery/menu/entity/Menu.java
@@ -1,9 +1,6 @@
 package com.example.jomajomadelivery.menu.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,17 +9,25 @@ import java.time.LocalDateTime;
 @Entity
 public class Menu {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_id")
     private Long menu_id;
     @ManyToOne
     @JoinColumn(name = "store_id")
     private Store store;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "price")
     private int price;
-    private String img_path;
+    @Column(name = "img_path")
+    private String imgPath;
     @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
     @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     public Menu(Store store, String name, String description, int price, String img_path) {
