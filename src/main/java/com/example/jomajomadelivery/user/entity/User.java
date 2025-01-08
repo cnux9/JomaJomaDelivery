992445050419951e7c377cf06ca8e299f0f0067b
Groo <woohyeon3382@naver.com>
2025-01-08 16:44:02 +0900

    feat: User 엔티티 구현

---
+++ src/main/java/com/example/jomajomadelivery/user/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.jomajomadelivery.user.entity;
+
+import com.example.jomajomadelivery.common.BaseEntity;
+import com.example.jomajomadelivery.user.dto.request.SignUpUserDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private String nickName;
+
+    private String phoneNumber;
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Address> addresses = new ArrayList<>(); Todo: Address 클래스 연결 필요
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    // JPA 스펙상 필요한 생성자
+    protected User() {
+    }
+
+    public User createUser(SignUpUserDto dto) {
+        UserBuilder user = User.builder()
+                .email(dto.email())
+                .password(dto.password())
+                .name(dto.name())
+                .role(dto.role())
+                .nickName(dto.nickname())
+                .phoneNumber(dto.phoneNumber());
+        return this;
+    }
+}
