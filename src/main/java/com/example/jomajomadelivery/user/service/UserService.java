a156c767dcf6c107d7fe9219b7e7760ad3cbece7
Groo <woohyeon3382@naver.com>
2025-01-08 16:50:52 +0900

    feat: User 서비스 구현

--- src/main/java/com/example/jomajomadelivery/user/service/UserService.java
+++ src/main/java/com/example/jomajomadelivery/user/service/UserService.java
@@ -1,4 +1,21 @@
 package com.example.jomajomadelivery.user.service;
 
+import com.example.jomajomadelivery.user.dto.request.SignUpUserDto;
+import com.example.jomajomadelivery.user.entity.User;
+import com.example.jomajomadelivery.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void saveUser(SignUpUserDto dto) {
+        User user = User.createUser(dto);
+
+        // Todo: Address 생성 및 적용
+        userRepository.save(user);
+    }
 }
