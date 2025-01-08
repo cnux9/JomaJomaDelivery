a0326164d5cf0f6155048eea9337d72b61fec334
cnux9 <cnux9@naver.com>
2025-01-08 10:41:36 +0900

    first commit

---
+++ src/main/java/com/example/jomajomadelivery/JomaJomaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jomajomadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JomaJomaDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JomaJomaDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jomajomadelivery/JomaJomaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jomajomadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JomaJomaDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
