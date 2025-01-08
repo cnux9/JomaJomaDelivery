de1d0bf61fff5e256e0bc0c1c42b9aa1a6533ebc
Groo <woohyeon3382@naver.com>
2025-01-08 19:46:37 +0900

    feat: UserController 구현

--- src/main/java/com/example/jomajomadelivery/user/controller/UserController.java
+++ src/main/java/com/example/jomajomadelivery/user/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.example.jomajomadelivery.user.controller;
 
+import com.example.jomajomadelivery.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp() {
+
+        // Todo: 회원가입은 소셜로그인을 통해 진행
+
+        return ResponseEntity.ok("");
+    }
 }
