38ed4f255dd0967cee33755f0f4d384c791117d1
Groo <woohyeon3382@naver.com>
2025-01-08 19:51:18 +0900

    refactor: 주석 추가

--- src/main/java/com/example/jomajomadelivery/store/service/StoreService.java
+++ src/main/java/com/example/jomajomadelivery/store/service/StoreService.java
@@ -1,10 +1,7 @@
 package com.example.jomajomadelivery.store.service;
 
 import com.example.jomajomadelivery.store.dto.request.StoreRequestDto;
-import com.example.jomajomadelivery.store.entity.Store;
 import com.example.jomajomadelivery.store.repository.StoreRepository;
-import com.example.jomajomadelivery.user.entity.Role;
-import com.example.jomajomadelivery.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +10,9 @@ import org.springframework.stereotype.Service;
 public class StoreService {
     private final StoreRepository storeRepository;
     public void addStore(StoreRequestDto dto) {
-        User user = new User(1L,"aa","aa","dd", Role.SELLER,"dd","dd",false);
-        Store store =Store.addStore(user,dto);
-        //Todo dto에서 세부 주소를 따로 받고 store 엔티티 수정해야함.
-        storeRepository.save(store);
+//        User user = new User(1L,"aa","aa","dd", Role.SELLER,"dd","dd",false);
+//        Store store =Store.addStore(user,dto);
+//        //Todo dto에서 세부 주소를 따로 받고 store 엔티티 수정해야함.
+//        storeRepository.save(store);
     }
 }
