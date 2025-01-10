dd00b1647e8ca98ef2891bfe9bed863764f4e39b
Groo <woohyeon3382@naver.com>
2025-01-11 00:06:19 +0900

    feat: 빈 record 객체 생성을 위한 empty 메서드 구현

--- src/main/java/com/example/jomajomadelivery/auth/dto/request/SignUpUserDto.java
+++ src/main/java/com/example/jomajomadelivery/auth/dto/request/SignUpUserDto.java
@@ -1,16 +1,24 @@
-package com.example.jomajomadelivery.user.dto.request;
+package com.example.jomajomadelivery.auth.dto.request;
+
+import com.example.jomajomadelivery.user.entity.Role;
 
 public record SignUpUserDto(
         String email,
         String password,
         String name,
+        String nickName,
+        String phoneNumber,
         String addressName,
         String zipcode,
         String state,
         String city,
         String street,
         String detailAddress,
-        String nickname,
-        String phoneNumber
+        Role role
 ) {
+    public static SignUpUserDto empty() {
+        return new SignUpUserDto(
+                "","","","","","","","","","","",Role.ROLE_USER
+        );
+    }
 }
