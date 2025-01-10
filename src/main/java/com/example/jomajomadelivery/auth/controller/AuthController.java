bc1e6b00ffb6884808a80422f4506b86e4c544cb
Groo <woohyeon3382@naver.com>
2025-01-10 16:58:24 +0900

    refactor: AuthController 수정

--- src/main/java/com/example/jomajomadelivery/auth/controller/AuthController.java
+++ src/main/java/com/example/jomajomadelivery/auth/controller/AuthController.java
@@ -2,15 +2,29 @@ package com.example.jomajomadelivery.auth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
 
     // Todo: 일반 로그인/회원가입 담당
 
+    /**
+     * 로그인 페이지
+     */
     @GetMapping("/login")
     public String loginPage() {
-        return "login";
+        return "login/login";
+    }
+
+    /**
+     * 로그인 처리
+     */
+    @PostMapping("/login")
+    public ModelAndView loginProcess(@RequestParam String username, @RequestParam String password) {
+        return new ModelAndView();
     }
 
     @GetMapping("/signup")
