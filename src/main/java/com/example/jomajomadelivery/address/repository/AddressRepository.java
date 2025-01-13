77044cac66268232c493e96b0d6d096c6283d819
Groo <woohyeon3382@naver.com>
2025-01-13 10:07:11 +0900

    refactor: 공백 제거

--- src/main/java/com/example/jomajomadelivery/address/repository/AddressRepository.java
+++ src/main/java/com/example/jomajomadelivery/address/repository/AddressRepository.java
@@ -5,5 +5,4 @@ import com.example.jomajomadelivery.address.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
 }
